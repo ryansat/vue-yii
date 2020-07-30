0b050a70f31055ec16b6f8de007ee210b1d6a740
Fajar Satria <fajarsat@hotmail.com>
2020-07-30 23:13:19 +0700

    add login

--- vuefrontend/src/router/index.js
+++ vuefrontend/src/router/index.js
@@ -1,29 +1,69 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import DefaultLayout from "../layouts/DefaultLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../views/Login";
+import Register from "../views/Register";
+// import authService from "../services/auth.service";
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Default',
+    redirect: '/home',
+    component: DefaultLayout,
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: Home
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/auth',
+    name: 'Auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: Login
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: Register
+      }
+    ]
+  },
+  {
+    path: '/login',
+    redirect: '/auth/login'
+  },
+  {
+    path: '/register',
+    redirect: '/auth/register'
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+// router.beforeEach((to, from, next) => {
+//   if (to.name === 'home' && !authService.isLoggedIn()) {
+//     next({name: 'login'})
+//   } else if (authService.isLoggedIn() && to.name !== 'home'){
+//     next({name: 'home'})
+//   } else {
+//     next();
+//   }
+// });
 
 export default router
